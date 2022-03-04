78df78362083543eb478c7bbf6545881f3ea620a
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-04 14:43:19 +0100

    Aggiungo l'età del passeggero alla console

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 console.log('Hello world')
 
+
+// Kilometraggio
+
 const kmDaPercorrere = prompt('Quanti chilometri devi percorrere?')
 
 console.log('Deve percorrere:', kmDaPercorrere, 'km')
+
+
+// età del passeggero
+
+const etàUser = prompt('Quanti anni hai?')
+
+console.log('Il passeggero ha', etàUser, 'anni')
